5b9313f7fb2b21ac5326a8ac91ce4c54beadfc53
Chen Kasirer <chen902@gmail.com>
2020-01-13 19:15:17 +0100

    created base class for programshader

--- Game01/ShaderProgram.cpp
+++ Game01/ShaderProgram.cpp
@@ -20,97 +20,6 @@ ShaderProgram::~ShaderProgram()
 {
 }
 
-unsigned int ShaderProgram::loadShader(const std::string & filepath, const GLenum & type)
-{
-	std::stringstream source;
-	
-	unsigned int shaderID = glCreateShader(type);
-	
-	std::fstream file;
-
-	try{
-		file.open(filepath);
-	}
-	catch (std::exception e){
-		logger.error(e.what());
-	}
-
-	std::string line;
-	
-	// read shader sourcer from file
-	while (std::getline(file, line)) {
-		source << line << "\n";
-	}
-	
-	file.close();
-
-	std::string str_source = source.str();
-	const char* source_c = str_source.c_str();
-
-	// bind shader source
-	glShaderSource(shaderID, 1, &source_c, NULL);
-
-	// compile current shader
-	glCompileShader(shaderID);
-	
-	int  success;
-	char infoLog[512];
-
-	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
-	
-	if (!success) {
-		glGetShaderInfoLog(shaderID, 512, NULL, infoLog);
-		logger.error("compiling shader failer!");
-		logger.error(infoLog);
-	}
-	else
-	{
-		logger.info("shader compilation successfull.");
-	}
-
-	return shaderID;
-}
-
-unsigned int ShaderProgram::createProgram()
-{
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShaderID);
-	glAttachShader(shaderProgram, fragmentShaderID);
-	glLinkProgram(shaderProgram);
-	
-	int success;
-	
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-
-	if (!success) {
-		char infoLog[512];
-		logger.error("linking shader program failed!");
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		logger.error(infoLog);
-		return -1;
-	}
-
-	// shaders should be deleted after linkage
-	glDeleteShader(vertexShaderID);
-	glDeleteShader(fragmentShaderID);
-
-	logger.debug("linked shader program successfully!");
-	
-	return shaderProgram;
-}
-
-void ShaderProgram::startShader()
-{
-	glUseProgram(programID);
-}
-
-void ShaderProgram::stopShader()
-{
-	glUseProgram(0);
-}
-
 void ShaderProgram::loadViewlMatrix(const Camera & camera) const
 {
 	glm::mat4 view = Transformations::createViewMatrix(camera);
@@ -132,16 +41,4 @@ void ShaderProgram::getAllUniformLocations()
 	locationModelMatrix = getUniformLocation("model_matrix");
 	locationViewMatrix = getUniformLocation("view_matrix");
 	locationProjectionMatrix = getUniformLocation("projection_matrix");
-}
-
-int ShaderProgram::getUniformLocation(const std::string & name)
-{
-	int location = glGetUniformLocation(programID, name.c_str());
-	return location;
-}
-
-/* Loads a matrix to uniform with location in shader program */
-void ShaderProgram::loadMatrix(int location, const glm::mat4 & matrix) const
-{
-	glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 }
--- Game01/ShaderProgram.h
+++ Game01/ShaderProgram.h
@@ -10,16 +10,13 @@
 #include <GL/glew.h>
 #include "Entity.h"
 #include "Transformations.hpp"
+#include "StaticShader.h"
 
-class ShaderProgram
+class ShaderProgram: public StaticShader
 {
 public:
 	ShaderProgram();
 	~ShaderProgram();
-	unsigned int loadShader(const std::string& filepath, const GLenum& type);
-	unsigned int createProgram();
-	void startShader();
-	void stopShader();
 	void loadViewlMatrix(const Camera& camera) const;
 	void loadModelMatrix(const glm::mat4& matrix) const;
 	void loadProjectionMatrix(const glm::mat4& matrix) const;
@@ -30,18 +27,12 @@ public:
 
 private:
 	Logger logger = Logger("ShaderProgram");
-	unsigned int programID;
-	unsigned int vertexShaderID;
-	unsigned int fragmentShaderID;
+
 	int locationModelMatrix;
 	int locationViewMatrix;
 	int locationProjectionMatrix;
 	
 	const char* VERTEX_SHADER_FILE = "vertexShader.txt";
 	const char* FRAGMENT_SHADER_FILE = "fragmentShader.txt";
-
-	// private methods
-	int getUniformLocation(const std::string& name);
-	void loadMatrix(int location, const glm::mat4& matrix) const;
 };
 
